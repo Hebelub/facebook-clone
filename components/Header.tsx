5a728739a19abd90c250b80860b55637f3fda250
Gabriel Løsnesløkken <gabriell@uia.no>
2023-12-26 17:55:10 +0100

    Create header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,6 +16,7 @@ import {
     MagnifyingGlassIcon,
     ShoppingCartIcon,
 } from '@heroicons/react/24/outline';
+import HeaderIcon from './HeaderIcon'
 
 function Header() {
     return (
@@ -32,7 +33,7 @@ function Header() {
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <MagnifyingGlassIcon className="h-6 text-gray-600" />
                     <input 
-                        className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" 
+                        className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" 
                         type="text" 
                         placeholder="Search Facebook" 
                     />
@@ -40,6 +41,15 @@ function Header() {
             </div>
 
             {/* Center */}
+            <div className="flex justify-center flex-grow">
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
+                </div>
+            </div>
 
             {/* Right */}
 
