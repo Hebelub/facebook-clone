8c81c4fd3a08eeada4260f75b065457d23ddba46
Gabriel Løsnesløkken <gabriell@uia.no>
2023-12-26 12:25:29 +0100

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
